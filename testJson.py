e5c5100b37d565e9021ced0fa2293de313f519b4
fcorno <fulvio.corno@polito.it>
2015-05-14 17:30:05 +0200

    Added 2 news APIs, just developed

--- testJson.py
+++ testJson.py
@@ -27,6 +27,36 @@ if __name__ == "__main__":
             print "\t%s -> %s: %s" % (item['start'], item['end'], item['text'] )
         r.close()
 
+    urlAule = "http://www.swas.polito.it/dotnet/orari_lezione_pub/mobile/ws_orari_mobile.asmx/get_elenco_aule"
+    parametriAule = {}
+
+    r = requests.post(urlAule,json=parametriAule)
+
+    aule = r.json()
+    print "Ci sono %d aule" % len(aule['d'])
+    for aula in aule['d']:
+        print "Aula %s (sede %s %s), tipo %s, posti %d, (%.3f,%.3f)" % (aula['aula'], aula['sede'], aula['sito'],
+                                                                        aula['tipo'], int(aula['posti']),
+                                                                        float(aula['lat'].replace(',', '.')),
+                                                                        float(aula['lon'].replace(',', '.')))
+
+
+    print
+    print "** ORARIO AULA 3I"
+
+    urlOrarioAula = "http://www.swas.polito.it/dotnet/orari_lezione_pub/mobile/ws_orari_mobile.asmx/get_orario_aula"
+    parametriOrarioAula = {'aula': '3I', 'datarif': '2015-05-14'}
+
+    r = requests.post(urlOrarioAula,json=parametriOrarioAula)
+
+    orarioAula = r.json()
+
+    #print orarioAula
+
+    for item in orarioAula['d']:
+        print "\t%s -> %s: %s" % (item['start'], item['end'], item['text'] )
+
+
 
 '''
 Metodo: http://www.swas.polito.it/dotnet/orari_lezione_pub/mobile/ws_orari_mobile.asmx/get_elenco_materie
@@ -60,4 +90,45 @@ $.ajax({
                 data: "{ 'listachiavimaterie': '" + param + "', 'datarif': '" + datarif_getorario.toDateString() + "'}"
 
             })
+'''
+
+
+'''
+METHOD
+get_elenco_aule
+parametro input: nessuno
+output: elenco aule con sede e coordinate GPS
+
+    public class objListaAule
+    {
+        public string aula { get; set; }
+        public string sede { get; set; }
+        public string sito { get; set; }
+        public string posti { get; set; }
+        public string tipo { get; set; }
+        public string lat { get; set; }
+        public string lon { get; set; }
+    }
+'''
+
+'''
+METHOD
+get_orario_aula
+parametri input: AULA, DATARIF
+output: orario dell'aula per la giornata
+
+    public class objListaEventi
+    {
+        public string id { get; set; }
+        public string text { get; set; }
+        public string start { get; set; }
+        public string end { get; set; }
+        public string tipo_evento { get; set; }
+        public string titolo_materia { get; set; }
+        public string nominativo_docente { get; set; }
+        public string desc_evento { get; set; }
+        public string aula { get; set; }
+    }
+
+
 '''
